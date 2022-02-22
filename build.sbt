c13a20f6f833915c3e2b4a76c5185620f7dd5691
Zack Balbin <zack@zackbalbin.com>
2022-02-22 01:27:38 -0800

    init repo

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := "ergo-names-sdk"
+
+scalaVersion := "2.13.8"
+
+libraryDependencies +=  "org.scalaj" %% "scalaj-http" % "2.4.2"
+libraryDependencies += "io.spray" %%  "spray-json" % "1.3.6"
---
+++ src/main/scala/Sdk.scala
@@ -0,0 +1,54 @@
+package ergonames.Sdk
+
+import scalaj.http._
+import spray.json._
+import DefaultJsonProtocol._
+
+object Sdk {
+
+  val ergonamesMainnetAPIUrl: String = "https://api.ergonames.com"
+  val ergonamesTestnetAPIUrl: String = "https://testnet-api.ergonames.com"
+
+  def createUrl(name: String): String = {
+    val url = ergonamesTestnetAPIUrl + "/ergonames/resolve/" + name
+    url
+  }
+
+  def makeRequest(url: String): String = {
+    val response = Http(url).asString
+    val body = response.body
+    body
+  }
+
+  def parseResponse(response: String): Map[String, Option[String]] = {
+    val json = response.parseJson
+    val mappedData = json.convertTo[Map[String, Option[String]]]
+    mappedData
+  }
+
+  def parseOption(raw: Option[String]): String = {
+    if (raw == None) {
+      "None"
+    } else {
+      raw.get
+    }
+  }
+
+  def getOwnerAddress(name: String): String = {
+    val url = createUrl(name)
+    val response = makeRequest(url)
+    val json = parseResponse(response)
+    val addressRaw = json.get("ergo").get
+    val address = parseOption(addressRaw)
+    address
+  }
+
+  def checkNameExists(name: String): Boolean = {
+    val address = getOwnerAddress(name)
+    if (address == "None") {
+      false
+    } else {
+      true
+    }
+  }
+}
